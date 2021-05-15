30f1ca0236609eb513fb987697ccad7461ff26ce
Manish Paryani <manishparyani@192.168.1.119>
2021-05-15 16:24:56 +0100

    second

--- pattern/src/main/java/SingleTonPattern.java
+++ pattern/src/main/java/SingleTonPattern.java
@@ -1,2 +1,78 @@
-public class SingleTonPattern {
+public class SingleTonPattern {}
+
+class EagerInitializedSingleton {
+
+  private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+  // private constructor to avoid client applications to use constructor
+  private EagerInitializedSingleton() {}
+
+  public static EagerInitializedSingleton getInstance() {
+    return instance;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(BillPughSingleton.getInstance());
+  }
+}
+
+class StaticBlockSingleton {
+
+  private static StaticBlockSingleton instance;
+
+  private StaticBlockSingleton() {}
+
+  // static block initialization for exception handling
+  static {
+    try {
+      instance = new StaticBlockSingleton();
+    } catch (Exception e) {
+      throw new RuntimeException("Exception occured in creating singleton instance");
+    }
+  }
+
+  public static StaticBlockSingleton getInstance() {
+    return instance;
+  }
+}
+
+class LazyInitializedSingleton {
+
+  private static LazyInitializedSingleton instance;
+
+  private LazyInitializedSingleton() {}
+
+  public static LazyInitializedSingleton getInstance() {
+    if (instance == null) {
+      instance = new LazyInitializedSingleton();
+    }
+    return instance;
+  }
+}
+
+class ThreadSafeSingleton {
+
+  private static ThreadSafeSingleton instance;
+
+  private ThreadSafeSingleton() {}
+
+  public static synchronized ThreadSafeSingleton getInstance() {
+    if (instance == null) {
+      instance = new ThreadSafeSingleton();
+    }
+    return instance;
+  }
+}
+
+class BillPughSingleton {
+
+  private BillPughSingleton() {}
+
+  private static class SingletonHelper {
+    private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+  }
+
+  public static BillPughSingleton getInstance() {
+    return SingletonHelper.INSTANCE;
+  }
 }
